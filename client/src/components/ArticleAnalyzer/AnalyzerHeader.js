894a517432285203b4d9d468c989976d86f18379
Xinran Luo <luoxinran28@gmail.com>
2025-01-27 12:20:18 +0800

    Use flex to center the tabs

--- client/src/components/ArticleAnalyzer/AnalyzerHeader.js
+++ client/src/components/ArticleAnalyzer/AnalyzerHeader.js
@@ -17,7 +17,7 @@ import {
   Upload as UploadIcon,
   Clear as ClearIcon
 } from '@mui/icons-material';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function AnalyzerHeader({ onUpload, onClearCache, sceneTitle, currentTab, onTabChange }) {
   const navigate = useNavigate();
@@ -47,77 +47,76 @@ function AnalyzerHeader({ onUpload, onClearCache, sceneTitle, currentTab, onTabC
 
   return (
     <AppBar position="static" color="default" elevation={1}>
-      <Box sx={{ position: 'relative' }}>
-        <Toolbar sx={{ justifyContent: 'space-between' }}>
-          {/* 左侧区域 */}
-          <Box sx={{ display: 'flex', alignItems: 'center', position: 'absolute', left: 16 }}>
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="back"
-              onClick={handleBack}
-            >
-              <ArrowBackIcon />
-            </IconButton>
-            <Typography variant="h6" sx={{ ml: 2 }}>
-              {sceneTitle}
-            </Typography>
-          </Box>
+      <Toolbar sx={{ 
+        display: 'grid',
+        gridTemplateColumns: '1fr auto 1fr',
+        alignItems: 'center',
+        minHeight: 64,
+        px: 2
+      }}>
+        {/* 左侧区域 */}
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="back"
+            onClick={handleBack}
+          >
+            <ArrowBackIcon />
+          </IconButton>
+          <Typography variant="h6" sx={{ ml: 2 }}>
+            {sceneTitle}
+          </Typography>
+        </Box>
 
-          {/* 中间的 Tabs */}
-          <Box sx={{ 
-            position: 'absolute', 
-            left: '50%', 
-            transform: 'translateX(-50%)',
-            zIndex: 1
-          }}>
-            <Tabs 
-              value={currentTab} 
-              onChange={onTabChange}
-              textColor="primary"
-              indicatorColor="primary"
-            >
-              <Tab label="文章" value="articles" />
-              <Tab label="概要" value="overview" />
-              <Tab label="分析" value="analysis" />
-            </Tabs>
-          </Box>
+        {/* 中间的 Tabs */}
+        <Box>
+          <Tabs 
+            value={currentTab} 
+            onChange={onTabChange}
+            textColor="primary"
+            indicatorColor="primary"
+          >
+            <Tab label="文章" value="articles" />
+            <Tab label="概要" value="overview" />
+            <Tab label="分析" value="analysis" />
+          </Tabs>
+        </Box>
 
-          {/* 右侧工具菜单 */}
-          <Box sx={{ position: 'absolute', right: 0 }}>
-            <IconButton
-              color="inherit"
-              onClick={handleToolsClick}
-              size="large"
-            >
-              <AppsIcon />
-            </IconButton>
-            <Menu
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-              }}
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-            >
-              <MenuItem onClick={handleUpload}>
-                <UploadIcon sx={{ mr: 1 }} />
-                上传内容
-              </MenuItem>
-              <Divider />
-              <MenuItem onClick={handleClearCache}>
-                <ClearIcon sx={{ mr: 1 }} />
-                清空数据
-              </MenuItem>
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Box>
+        {/* 右侧工具菜单 */}
+        <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+          <IconButton
+            color="inherit"
+            onClick={handleToolsClick}
+            size="large"
+          >
+            <AppsIcon />
+          </IconButton>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+            <MenuItem onClick={handleUpload}>
+              <UploadIcon sx={{ mr: 1 }} />
+              上传内容
+            </MenuItem>
+            <Divider />
+            <MenuItem onClick={handleClearCache}>
+              <ClearIcon sx={{ mr: 1 }} />
+              清空数据
+            </MenuItem>
+          </Menu>
+        </Box>
+      </Toolbar>
     </AppBar>
   );
 }
